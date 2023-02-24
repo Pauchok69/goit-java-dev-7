38c61757797ca3314aa8e8c8f56107dd6919807a
Andy <andrew.zerko@gmail.com>
2023-02-24 09:47:24 +0200

    #2. Start implementing HttpStatusImageDownloader

---
+++ src/main/java/org/andy/HttpStatusImageDownloader.java
@@ -0,0 +1,15 @@
+package org.andy;
+
+import java.net.URI;
+
+public class HttpStatusImageDownloader {
+    private final HttpStatusChecker httpStatusChecker;
+
+    public HttpStatusImageDownloader() {
+        this.httpStatusChecker = new HttpStatusChecker();
+    }
+
+    public void downloadStatusImage(int code) {
+        URI.
+    }
+}
